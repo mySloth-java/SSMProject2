96b60690c7309b9dbd53bd38b4569340a2d76cee
zero <cg2100773258@163.com>
2022-09-23 19:59:39 +0800

    LoginAndSelect

---
+++ src/main/java/com/cg/Controller/SelectCon.java
@@ -0,0 +1,23 @@
+package com.cg.Controller;
+
+import com.cg.Dao.TestMapper;
+import com.cg.pojo.employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+public class SelectCon {
+    @Autowired
+    TestMapper mapper;
+
+    @RequestMapping(value = "/SelectEmp",method = RequestMethod.GET)
+    public String SelectEmp(Model model){
+        //调取查询方法，设置共享域
+        List<employee> list = mapper.getAll();
+        model.addAttribute("allEmp", list);
+        return "empSelect";
+    }
+}
--- src/main/java/com/cg/Controller/TestCon.java
+++ src/main/java/com/cg/Controller/TestCon.java
@@ -4,11 +4,9 @@ import com.cg.Dao.TestMapper;
 import com.cg.pojo.employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -20,15 +18,13 @@ public class TestCon {
     @Autowired
     TestMapper testMapper;
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public String getNameEmp(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String getNameEmp(HttpServletRequest request, HttpServletResponse response) throws IOException {
         //获取登录的信息
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         List<employee> nameEmp = testMapper.getNameEmp(username,password);
         System.out.println(nameEmp);
-        //设置共享域
-        model.addAttribute("user",nameEmp);
         //给予登录反馈
         if (nameEmp.isEmpty()){
             response.getWriter().write("登录失败");
@@ -36,6 +32,7 @@ public class TestCon {
         }else {
             return "LoginSuccess";
         }
+
 
 
     }
