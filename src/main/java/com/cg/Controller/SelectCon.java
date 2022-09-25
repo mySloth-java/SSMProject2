da6d9e96415e093d0b091b6863014215254e18bc
zero <cg2100773258@163.com>
2022-09-25 22:32:12 +0800

    LoginCss

--- src/main/java/com/cg/Controller/SelectCon.java
+++ src/main/java/com/cg/Controller/SelectCon.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.ValidationEvent;
 import java.util.List;
 @Controller
 public class SelectCon {
@@ -20,6 +22,23 @@ public class SelectCon {
         List<employee> list = mapper.getAll();
         model.addAttribute("allEmp", list);
         return "empSelect";
+    }
+
+    //添加功能仅跳转，在控制器内
+    @RequestMapping(value = "/addEmp",method = RequestMethod.POST)
+    public String AddEmp(HttpServletRequest request){
+        //获取填入的信息
+        String empID = request.getParameter("empID");
+        int id = Integer.parseInt(empID);
+        String empName = request.getParameter("empName");
+        String age = request.getParameter("age");
+        Integer integer = Integer.getInteger(age);
+        String sex = request.getParameter("sex");
+        String email = request.getParameter("email");
+
+
+        mapper.AddEmp(id,empName,integer,sex,email);
+        return "redirect:empSelect";
     }
 
 }
