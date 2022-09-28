0626e5b1bad6a8dff37816c3d85fe9d9f1e66eb3
zero <cg2100773258@163.com>
2022-09-28 09:20:54 +0800

    LoginAdd

--- src/main/java/com/cg/Controller/SelectCon.java
+++ src/main/java/com/cg/Controller/SelectCon.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.bind.ValidationEvent;
 import java.util.List;
 @Controller
 public class SelectCon {
