b5b42af974cde53cf61dd467827ab175a937cacd
zero <cg2100773258@163.com>
2022-09-28 14:55:08 +0800

    errorUpdate

--- src/main/java/com/cg/Controller/SelectCon.java
+++ src/main/java/com/cg/Controller/SelectCon.java
@@ -9,29 +9,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 @Controller
-public class SelectCon {
-    @Autowired
-    TestMapper mapper;
-
-    @RequestMapping(value = "/SelectEmp",method = RequestMethod.GET)
-    public String SelectEmp(Model model){
-        //调取查询方法，设置共享域
-        List<employee> list = mapper.getAll();
-        model.addAttribute("allEmp", list);
-        return "empSelect";
-    }
-
-    //添加功能仅跳转，在控制器内
-    @RequestMapping(value = "/addEmp",method = RequestMethod.POST)
-    public String AddEmp(employee employee){
-        mapper.AddEmp(employee);
-        return "redirect:/SelectEmp";
-    }
-
-    @RequestMapping(value = "/updateEmp")
-    public String UpdateEmp(employee employee){
-        mapper.UpdateEmp(employee);
-        return "redirect:/SelectEmp";
-    }
-
-}
+public class SelectCon {}
